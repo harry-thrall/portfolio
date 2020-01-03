5f0093e8de9ac647638d3e906117e9621fb561a3
harry-thrall <52261914+harry-thrall@users.noreply.github.com>
2020-01-03 03:43:02 +0000

    Updated all files to php, added files for project pages

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="A front-end web designer and developer based in North Yorkshire, UK. Creating beautiful, bespoke and unique websites for both individuals or businesses">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Catamaran|Lora|Open+Sans|Pacifico|Dancing+Script" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/db9af42d68.js"></script>
+    <title>Harry Thrall - Front End Developer</title>
+  </head>
+  <body>
+    <div class="page-container">
+      <header>
+        <a href="#" class="header-logo">Harry Thrall</a>
+        <input type="checkbox" class="toggle" id="checkbox">
+        <nav id="nav">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#about">About Me</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+        <label for="checkbox" class="nav-toggle">
+          <div class="hamburger"><div></div></div>
+        </label>
+      </header>
